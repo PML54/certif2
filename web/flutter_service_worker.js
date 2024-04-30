e8a2c89f63cc5b65ddd5e7ab5ab036ffc862668d
Paul de Pontoise <35325632+PML54@users.noreply.github.com>
2024-04-30 07:04:15 +0200

    Add files via upload

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "efbe081cad2e9e65f366e4c9f8a11784",
-"index.html": "f04e8a4cd67dd5dc4693c4cb3072e41d",
-"/": "f04e8a4cd67dd5dc4693c4cb3072e41d",
-"main.dart.js": "47e059ba448d81e445866e6aaa5f2252",
+"index.html": "043aa78c9f009594218f46e9a6e27fdb",
+"/": "043aa78c9f009594218f46e9a6e27fdb",
+"main.dart.js": "9f012f34f113aa44d92bc00971bd4892",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
